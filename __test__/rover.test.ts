0c5c6a0c1695c2ac2b0862e921b0c8093c312890
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-04-26 10:03:13 +0100

    Map class created and use cases determinated

--- __test__/rover.test.ts
+++ __test__/rover.test.ts
@@ -1,8 +1,38 @@
 import { Rover } from '../src/rover';
 
+/*
+
+  - Hace falta determinar el mapa
+  ¿Quien conoce a quien?
+  - El rovers conoce el mapa
+
+ Casos de uso ->
+
+  Para crear el mapa, string de una linea: "numero,numero"
+    1. Comprobar que son números
+    2. Comprobar que los números son mayores o iguales a 0
+
+  Para crear un rovers se envía el mapa creado.
+
+  Para ejecutar los comandos de un rovers, estos llegan como un string de dos lineas: posición y movimientos por hacer
+    1. Comprobar que la primera linea siga la siguiente estructura -> "number,number,direction"
+    2. Comprobar que la segunda línea contenga movimientos correctos. any is equal to some (R,L,M)
+    3. Si todo es correcto, se crea el objeto con los datos correctos.
+  
+  ¿Cómo comprobar que el rovers está en el límite del mapa y qué hacer?
+    1. Crear dos métodos, uno que compruebe la posición en x y otro en y. Se le envía el valor que se le vaya a
+       sumar/restar y en caso de "salga" del mapa, se devuelve un true indicando que se va a salir del mapa
+    
+
+*/
+
 describe('Mars Rover test', () => {
   it('should be instantiated', () => {
     const rover = new Rover();
     expect(rover).toBeTruthy()
   });
+
+
 });
+
+
---
+++ src/map.ts
@@ -0,0 +1,11 @@
+export class Map{
+    max_x: number;
+    min_x = 0
+    min_y = 0;
+    max_y: number
+
+    constructor(x: number, y: number){
+        this.max_x = x;
+        this.max_y = y
+    }
+}
